6d97c76ef027d8d328d673a66f12e31d71705036
SHIR0N3K0 <evanleites@yahoo.com>
2024-12-02 14:57:59 +0100

    start Ennemy setup

--- ProjetAventutureCPP/enemies/enemy.cpp
+++ ProjetAventutureCPP/enemies/enemy.cpp
@@ -1,1 +1,46 @@
 #include "enemy.h"
+#include <iostream>
+
+
+enemy::enemy()
+{
+    std::cout << "constructor" << "\n";
+}
+
+enemy::~enemy()
+{
+    std::cout << "destructor" << "\n";
+}
+
+std::string enemy::getName()
+{
+    return name;
+}
+
+void enemy::setName(const std::string& n)
+{
+    if (name.empty())
+        name = "DEFAULT";
+    else name = n;
+}
+
+void enemy::setmaxhealth(int m_maxhealth)
+{
+    maxhealth = m_maxhealth < 0 ? 0 : m_maxhealth;
+}
+
+void enemy::sethealth(float h_health)
+{
+    health = h_health;
+}
+
+void enemy::setattack(float a_attack)
+{
+    attack = a_attack;
+}
+
+void enemy::setdefense(float d_defense)
+{
+    defense = d_defense;
+}
+
