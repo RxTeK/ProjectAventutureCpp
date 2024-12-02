0798587e0d62cdf21f59fa7fa3230264a618faf4
RxTeK <theodep34@gmail.com>
2024-12-02 17:07:03 +0100

    push evan

--- ProjetAventutureCPP/enemies/enemy.h
+++ ProjetAventutureCPP/enemies/enemy.h
@@ -3,31 +3,7 @@
 
 class enemy
 {
-protected:
-    std::string name;
-    int maxhealth = 0;
-    float health = 0.0;
-    float attack = 0.0;
-    float defense = 0.0;
-    bool alive;
     
 public:
-    
-    enemy ();
-    virtual ~enemy();
 
-    std::string getName();
-    void setName(const std::string& name);
-    
-    virtual int getmaxhealth() = 0;
-    void setmaxhealth(int m_maxhealth);
-    
-    virtual float getheath() = 0;
-    void sethealth(float h_health);
-
-    virtual float getattack() = 0;
-    void setattack(float a_attack);
-
-    virtual float getdefense() = 0;
-    void setdefense(float d_defense);
 };
