807da8b49c667ac789388e9a4964d07d65d828d6
RxTeK <theodep34@gmail.com>
2024-12-10 22:59:06 +0100

    prbl de room resolu !!!!

--- ProjetAventutureCPP/Game/ProjetAventutureCPP.cpp
+++ ProjetAventutureCPP/Game/ProjetAventutureCPP.cpp
@@ -39,28 +39,24 @@ void initializeRooms(std::vector<Room*>& rooms)
         switch (randomVal)
         {
         case 1:
-            rooms.push_back(&littleRoom);
+            rooms.push_back(new LittleRoom());
             break;
         case 2:
-            rooms.push_back(&intermediateRoom);
+            rooms.push_back(new IntermediateRoom());
             break;
         case 3:
-            rooms.push_back(&largeRoom);
+            rooms.push_back(new LargeRoom());
             break;
         }
     }
-    rooms.push_back(&bossRoom);
-    for (size_t i = 0; i < rooms.size(); ++i)
-    {
-        std::cout << rooms[i]->getName() << std::endl;
-    }
+    //rooms.push_back(&bossRoom);
 }
 void fight(Player* player, Perso* enemy, bool& isDead)
 {
     isDead = false;
 
     // Affichage du joueur et de l'ennemi
-    system("cls");
+    //system("cls");
     std::cout << *player;
     Sleep(500);
     std::cout << *enemy << "\n";
@@ -69,7 +65,7 @@ void fight(Player* player, Perso* enemy, bool& isDead)
     // Tour du joueur
     std::cout << "Player attack turn:\n1.Punch      2.Slash\n";
     std::cin >> player->attackNumber;
-    system("cls");
+    //system("cls");
     player->setchooseAttack(player->attackNumber);
     player->setattackType(player->getattackName(), *enemy);
 
@@ -83,7 +79,7 @@ void fight(Player* player, Perso* enemy, bool& isDead)
 
     // Tour de l'ennemi
     Sleep(2000);
-    system("cls");
+    //system("cls");
     enemy->setattackType("Head butt", *player);
 
     // Vérifie si le joueur est mort
@@ -94,13 +90,13 @@ void fight(Player* player, Perso* enemy, bool& isDead)
     }
 
     Sleep(2000);
-    system("cls");
+    //system("cls");
 }
 int main()
 {
     Player p1_obj;
     Player* p1 = &p1_obj;
-    std::vector<Perso*> monsters;
+    std::vector<enemy*> monsters;
     monsters.push_back(new Slime());
     monsters.push_back(new Gobelin());
     monsters.push_back(new Slime());
@@ -112,59 +108,55 @@ int main()
     while (true)
     {
         char c;
-        bool isDead = false;
         std::cout << "Press 'q' to play, 'd' to quit): ";
         std::cin >> c;
         switch (c)
         {
         case KEY_LEFT:
             // Room
-                while(rooms.size() > 0)
+                for (size_t i = 0; i < rooms.size(); ++i)
                 {
-                    std::cout << rooms.back()->getName() << "\n";
-                    std::cout << "Room 1: " << rooms[0]->getnumberEnemy() << " enemies." << std::endl;
-                  
-                    while (rooms[0]->getnumberEnemy() > 0)
-                    {
-                       
-                        if (!monsters.empty())
-                        {
-                          
-                            enemy* lastEnemy = dynamic_cast<enemy*>(monsters.back());
-                            if (lastEnemy)
-                            {
-                                bool enemyIsDead = false;
-                                fight(p1, lastEnemy, enemyIsDead);
-                                if (enemyIsDead)
-                                {
-                                    std::cout << "An enemy is dead." << std::endl;
-                                    monsters.pop_back();
-                                    delete lastEnemy;
-                                    rooms[0]->setnumberEnemy(rooms[0]->getnumberEnemy() - 1);
-                                }
-                            }
-                            else
-                            {
-                                std::cout << "Error: Monster is not of type enemy.\n";
-                                break;
-                            }
-                        }
-                        else
-                        {
-                            // NON : Il n'y a plus d'ennemis
-                            std::cout << "No more enemies to fight in this room.\n";
-                            break;
-                        }
-                    }
-
-                    if (rooms[0]->getnumberEnemy() == 0)
-                    {
-                        std::cout << "Room cleared!\n";
-                    }
-
-                    // Enlève la salle après le combat
-                    rooms.erase(rooms.begin());  // Supprimer la salle actuelle après l'avoir terminée
+                    std::cout << rooms[i]->getName() << std::endl;
                 }
+            while(!rooms.empty())
+            {
+                std::cout << rooms.back()->getName() << "\n";
+                std::cout << "Room 1: " << rooms[0]->getnumberEnemy() << " enemies." << std::endl;
+                  
+                while (rooms[0]->getnumberEnemy() > 0)
+                {
+                       
+                    if (!monsters.empty())
+                    {
+                        enemy* lastEnemy = monsters.back();
+                        bool enemyIsDead = false;
+                        fight(p1, lastEnemy, enemyIsDead);
+                        
+                        if (enemyIsDead)
+                        {
+                            std::cout << "An enemy is dead." << std::endl;
+                            monsters.pop_back();
+                            delete lastEnemy;
+                            rooms[0]->setnumberEnemy(rooms[0]->getnumberEnemy() - 1);
+                        }
+                    }
+                    else
+                    {
+                        // NON
+                        std::cout << "No more enemies to fight in this room.\n";
+                        break;
+                    }
+                }
+
+                if (rooms[0]->getnumberEnemy() == 0)
+                {
+                    std::cout << "Room cleared!\n";
+                }
+                    
+                rooms.erase(rooms.begin());
+                break;
+            }
+            break;
 
             // Quit
         case KEY_RIGHT:
